26aaf9cace997b0eec0d3f33b7060b2d381aa5d1
Swizec Teller <swizec@swizec.com>
2016-09-18 14:35:02 -0700

    Slack bot sends messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+let _ = require('lodash');
+
+let settings = require('../settings'),
+    APItoken = settings.slack_api_token;
+
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+let RtmClient = require('@slack/client').RtmClient;
+let rtm = new RtmClient(APItoken, {logLevel: 'debug'});
+rtm.start();
+
+let CHANNELS = [],
+    findChannel = (name) => _.find(CHANNELS, {name: name});
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+    CHANNELS = rtmStartData.channels;
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    rtm.sendMessage('this is a test', findChannel('bot-testing').id, () => {
+
+        console.log('message sent');
+
+
+        res.render('index', { title: 'Express' });
+    });
 });
 
 module.exports = router;
