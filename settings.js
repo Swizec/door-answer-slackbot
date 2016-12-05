f5aad45ae8e42a4614ed4f08d33b81413fe2de27
Swizec Teller <swizec@swizec.com>
2016-12-04 16:06:48 -0800

    Another mistake

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     slack: {
-        APIkey: process.env.SLACK_API_TOKEN || "xoxb-81007518101-qMgnGLW6WMbewe0kk3d3AXNd",
+        APItoken: process.env.SLACK_API_TOKEN || "xoxb-81007518101-qMgnGLW6WMbewe0kk3d3AXNd",
         oauthKey: process.env.SLACK_AOUTH_KEY || '14110144963.81022664631',
         oauthSecret: process.env.SLACK_OAUTH_SECRET || '24df277cd8e3d24e32087885c6ee7c80',
         channel: process.env.SLACK_CHANNEL || '#bot-testing'
